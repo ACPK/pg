cf7016fffedac6382303faa3a3711f0f8661725e
Derek Sivers <derek@sivers.org>
2016-03-09 13:59:03 +1300

    simplify currency test

--- currencies/sql.sql
+++ currencies/sql.sql
@@ -6,21 +6,12 @@ CREATE SCHEMA sivers;
 CREATE TYPE currency AS ENUM ('USD', 'EUR', 'JPY', 'BTC');
 CREATE TYPE currency_amount AS (currency currency, amount numeric);
 
-CREATE TABLE currencies (
-	code currency primary key
-);
-
 CREATE TABLE currency_rates (
 	code currency NOT NULL,
 	day date NOT NULL DEFAULT current_date,
 	rate numeric,
 	PRIMARY KEY (code, day)
 );
-
-INSERT INTO currencies (code) VALUES ('USD');
-INSERT INTO currencies (code) VALUES ('EUR');
-INSERT INTO currencies (code) VALUES ('JPY');
-INSERT INTO currencies (code) VALUES ('BTC');
 
 INSERT INTO currency_rates (code, day, rate) VALUES ('USD', '2015-09-12', 1);
 INSERT INTO currency_rates (code, day, rate) VALUES ('EUR', '2015-09-12', 0.881602);
@@ -31,13 +22,11 @@ INSERT INTO currency_rates (code, day, rate) VALUES ('BTC', '2015-09-12', 0.0043
 CREATE OR REPLACE FUNCTION update_currency_rates(jsonb) RETURNS void AS $$
 DECLARE
 	rates jsonb;
-	acurrency currencies;
 	acode currency;
 	arate numeric;
 BEGIN
 	rates := jsonb_extract_path($1, 'rates');
-	FOR acurrency IN SELECT * FROM currencies LOOP
-		acode := acurrency.code;
+	FOR acode IN SELECT code FROM currency_rates LOOP
 		arate := CAST((rates ->> acode::text) AS numeric);
 		INSERT INTO currency_rates (code, rate) VALUES (acode, arate);
 	END LOOP;
